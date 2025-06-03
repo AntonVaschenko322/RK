01aa73d9a7a21b847f2e6b7b3925210647905ebe
AntonVaschenko322 <antonvaschenko6@gmail.com>
2025-06-03 12:20:24 +0300

    cleared

--- test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/antonvaschenko/RK/test
-# Build directory: /home/antonvaschenko/RK/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
